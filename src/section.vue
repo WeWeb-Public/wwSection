4641d1fc921a48be8d4ad930f94410a46543e0a6
Kramerbaf <nds.kram@gmail.com>
2018-05-29 17:53:01 +0200

    first commit

---
+++ src/section.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://github.com/vuejs/vue-cli/tree/dev/docs" target="_blank">vue-cli documentation</a>.
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
+    </ul>
+    <h3 @click="coucou" >Essential Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    </ul>
+    <h3 @click="lala" >Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org/en/essentials/getting-started.html" target="_blank">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org/en/intro.html" target="_blank">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+    <wwObject v-bind:ww-object="section.data.title"></wwObject>
+  </div>
+</template>
+
+<script>
+let a = {
+  name: "section_A",
+  props: {
+    msg: String,
+    section: Object
+  },
+  methods: {
+    lala: function() {
+      console.log("lala");
+    }
+  }
+};
+
+/* wwManager:start */
+const managerFunctions = require("./manager.js").default;
+for (const key in managerFunctions) {
+  if (a.methods[key]) {
+    console.log("Function already exists : ", key);
+  } else {
+    a.methods[key] = managerFunctions[key];
+  }
+}
+/* wwManager:end */
+
+console.log(a.methods);
+
+export default a;
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+  color: red;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
