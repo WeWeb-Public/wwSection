4b65c1d2ecce6469db14335a0179ae2bceda92b9
Kramerbaf <nds.kram@gmail.com>
2018-10-31 00:47:28 +0100

    demo

--- src/section.vue
+++ src/section.vue
@@ -1,4 +1,3 @@
-
 <!-- This is a Vue.js single file component. -->
 <!-- Check the Vue.js doc here :  -->
 <!-- https://vuejs.org/v2/guide/ -->
@@ -9,6 +8,17 @@
 
         <!-- Weweb Wallpaper -->
         <wwObject class="background" v-bind:ww-object="section.data.wewebWP" v-bind:section="section" ww-category="background"></wwObject>
+
+        <!--
+            <div class="movingBg" v-bind:class="{'fixed': progress > 0}">
+                <wwObject class="background" v-bind:ww-object="section.data.wewebWP" v-bind:section="section" ww-category="background"></wwObject>
+                <div class="overlay" v-bind:style="opacity"></div>
+                <div class="border vert left" v-bind:style="vertScale"></div>
+                <div class="border vert right" v-bind:style="vertScale"></div>
+                <div class="border hor top" v-bind:style="horScale"></div>
+                <div class="border hor bottom" v-bind:style="horScale"></div>
+            </div>
+        -->
 
         <div class="content">
             <!-- Hello world ! -->
@@ -29,6 +39,32 @@ export default {
         // Use it has you like !
         section: Object
     },
+    /*/
+        data() {
+            return {
+                progress: 0
+            }
+        },
+        
+        computed: {
+            vertScale() {
+                return {
+                    transform: 'scale(' + (1 - this.progress) + ', 1)'
+                }
+            },
+            horScale() {
+                return {
+                    transform: 'scale(1,' + (1 - this.progress) + ')'
+                }
+            },
+            opacity() {
+                return {
+                    opacity: this.progress
+                }
+            }
+
+        },
+    /**/
     created() {
 
         //Initialize section data
@@ -60,9 +96,31 @@ export default {
             });
         }
 
+        /*/
+            window.addEventListener('scroll', this.onScroll);
+        /**/
     },
-    methods: {
-    }
+    /*/
+        methods: {
+            onScroll(event) {
+                const sectionTop = this.$el.getBoundingClientRect().y,
+                    clientHeight = document.documentElement.clientHeight;
+
+                if (sectionTop > 0) {
+                    this.progress = 0;
+                }
+                else if (sectionTop <= 0 && clientHeight * 2 - sectionTop > 0) {
+                    this.progress = Math.max(Math.min(-sectionTop / clientHeight * 2, 1), 0);
+                }
+                else {
+                    this.progress = 1;
+                }
+            }
+        },
+        beforeDestroy() {
+            window.removeEventListener('scroll', this.onScroll);
+        }
+    /**/
 };
 </script>
 
@@ -85,4 +143,79 @@ export default {
 .content {
   position: relative;
 }
+
+/*/
+    .hello {
+        flex-basis: 200%;
+    }
+
+    .content {
+    position: absolute;
+    top: 50%;
+    left: 150px;
+    }
+
+    .movingBg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 50%;
+    width: 100%;
+    }
+
+    .movingBg.fixed {
+    top: 0;
+    bottom: 0;
+    height: auto;
+    position: fixed;
+    }
+
+    .border {
+    background-color: white;
+    position: absolute;
+    z-index: 2;
+    }
+
+    .border.hor {
+    left: 0;
+    width: 100%;
+    height: 75px;
+    }
+
+    .border.vert {
+    top: 0;
+    height: 100%;
+    width: 75px;
+    }
+
+    .border.top {
+    top: 0;
+    transform-origin: 50% 0;
+    }
+
+    .border.bottom {
+    bottom: 0;
+    transform-origin: 50% 100%;
+    }
+
+    .border.left {
+    left: 0;
+    transform-origin: 0 50%;
+    }
+
+    .border.right {
+    right: 0;
+    transform-origin: 100% 50%;
+    }
+
+    .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.2);
+    z-index: 1;
+    }
+/**/
 </style>
